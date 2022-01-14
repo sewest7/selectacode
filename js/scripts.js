9467223faaba8bdc518a0dc3cbe31b6248b75199
sewest7 <seth.west7@gmail.com>
2022-01-14 15:26:56 -0800

    Add background image, center all text, add margins.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,12 @@ $(document).ready(function() {
     $("#bridgeKeeper").hide();
     $("#lancelotDiv").hide();
     $("#arthurDiv").hide();
- 
 
     const userCoconut = $("select#userCoconut").val();
     const userQuest = $("select#userQuest").val();
     const userColor = $("select#userColor").val();
     const userNext = $("select#userNext").val();
-
-    // let codeMatch === userCoconut && userColor && userNext && userQuest;
+    const userName = $("input#userName").val();
 
     if (userCoconut === 'found' && userQuest === 'camelot') {
       $("#concordeDiv").toggle();
@@ -24,64 +22,8 @@ $(document).ready(function() {
       $("#arthurDiv").toggle();
     }
 
-    // $("#theAnswer").text(codeMatch);
     $("#codeMatch").show();
     
-      
-    
-    // if (userCoconut === 'found' && userQuest === 'seekGrail' && userColor === 'colorBlue' && userNext === 'velocity') {
-    //   $("#rabbitDiv").show();
-    // } 
-    
-    // if (userCoconut === 'matter' && userQuest === 'seekGrail' && userColor === 'colorBlue' && userNext === 'velocity') {
-    //   $("#arthurDiv").show();
-    // } 
-    
-    // if (userCoconut === 'matter' && userQuest === 'seekGrail' && userColor === 'colorBlue' && userNext === 'assyria') {
-    //   $("#grailDiv").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'seekGrail' && userColor === 'notBlue' && userNext === 'assyria') {
-    //   $("#witchDiv").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'camelot' && userColor === 'notBlue' && userNext === 'assyria') {
-    //   $("#gallahadDiv").show();
-    // }
-    
-    // if (userCoconut === 'found' && userQuest === 'seekGrail' && userColor === 'colorBlue' && userNext === 'assyria') {
-    //   $("#robinDiv").show();
-    // }
-    
-    // if (userCoconut === 'found' && userQuest === 'seekGrail' && userColor === 'notBlue' && userNext === 'assyria') {
-    //   $("#zootDiv").show();
-    // }
-    
-    // if (userCoconut === 'found' && userQuest === 'seekGrail' && userColor === 'notBlue' && userNext === 'velocity') {
-    //   $("#blackKnight").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'seekGrail' && userColor === 'notBlue' && userNext === 'velocity') {
-    //   $("#godDiv").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'camelot' && userColor === 'notBlue' && userNext === 'velocity') {
-    //   $("#timDiv").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'camelot' && userColor === 'colorBlue' && userNext === 'velocity') {
-    //   $("#niKnight").show();
-    // }
-    
-    // if (userCoconut === 'matter' && userQuest === 'camelot' && userColor === 'colorBlue' && userNext === 'assyria') {
-    //   $("#bedevereDiv").show();
-    // }
-    
-    // if (userCoconut === 'found' && userQuest === 'camelot' && userColor === 'notBlue' && userNext === 'assyria') {
-    //   $("#frenchDiv").show();
-    // }
-
-
   });
 });
 
